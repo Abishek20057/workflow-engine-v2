f8fa0584df0f3ef0f824ea16c4497e56b0bff0ed
Abishek <yourgithubemail@gmail.com>
2026-03-17 15:48:22 +0530

    final UI fix + workflow list working

--- static/script.js
+++ static/script.js
@@ -1,3 +1,36 @@
+async function loadWorkflows() {
+    let res = await fetch("/get_workflows");
+    let data = await res.json();
+
+    let table = document.getElementById("workflowTable");
+    let exec = document.getElementById("execWorkflow");
+
+    table.innerHTML = `
+    <tr>
+        <th>Workflow Name</th>
+        <th>Action</th>
+    </tr>`;
+
+    exec.innerHTML = "";
+
+    data.forEach(w => {
+        table.innerHTML += `
+        <tr>
+            <td>${w.name}</td>
+            <td>
+                <button onclick="startWorkflow('${w.id}')">Start</button>
+            </td>
+        </tr>`;
+
+        exec.innerHTML += `<option value="${w.id}">${w.name}</option>`;
+    });
+}
+
+function startWorkflow(id) {
+    showTab('start');
+    document.getElementById("execWorkflow").value = id;
+}
+
 function showTab(tab) {
     document.querySelectorAll(".tab").forEach(t => t.classList.add("hidden"));
     document.getElementById(tab).classList.remove("hidden");
@@ -7,36 +40,13 @@ function viewHistory() {
     window.location = "/history";
 }
 
-// LOAD WORKFLOWS
-async function loadWorkflows() {
-    let res = await fetch("/get_workflows");
-    let data = await res.json();
-
-    let table = document.getElementById("workflowTable");
-    let exec = document.getElementById("execWorkflow");
-
-    table.innerHTML = "<tr><th>Name</th><th>Action</th></tr>";
-    exec.innerHTML = "";
-
-    data.forEach(w => {
-        table.innerHTML += `
-        <tr>
-            <td>${w.name}</td>
-            <td><button onclick="run('${w.id}')">Start</button></td>
-        </tr>`;
-
-        exec.innerHTML += `<option value="${w.id}">${w.name}</option>`;
-    });
-}
-
-// CREATE
 async function createWorkflow() {
     let name = document.getElementById("wfName").value;
+
     await fetch("/workflow?name=" + name, {method: "POST"});
     loadWorkflows();
 }
 
-// RUN
 function runWorkflow() {
     let wf = document.getElementById("execWorkflow").value;
     let amount = document.getElementById("amount").value;
